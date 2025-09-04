d0a9aa13a9546ecbc45af9054e3086c4e0525f23
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-04 22:38:30 +0300

    Added read_root func

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 
 app = FastAPI(
     title="URL Shortener",
 )
+
+
+@app.get("/")
+def read_root(
+    request: Request,
+    name: str = "World",
+):
+    docs_url = request.url.replace(
+        path="/docs",
+    )
+    return {
+        "message": f"Hello {name}",
+        "docs": str(docs_url),
+    }
