533fbc185a8e436f127fe28fa24a7d339146ff21
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-10 20:33:29 +0300

    Added ShortUrlCreate scheme

--- url-shortener/schemas/short_url.py
+++ url-shortener/schemas/short_url.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel, AnyHttpUrl
 
+from typing import Annotated
+
+from annotated_types import Len
+
 
 class ShortUrlBase(BaseModel):
     target_url: AnyHttpUrl
@@ -8,3 +12,10 @@ class ShortUrlBase(BaseModel):
 
 class ShortUrl(ShortUrlBase):
     pass
+
+
+class ShortUrlCreate(ShortUrlBase):
+    slug: Annotated[
+        str,
+        Len(min_length=3, max_length=10),
+    ]
