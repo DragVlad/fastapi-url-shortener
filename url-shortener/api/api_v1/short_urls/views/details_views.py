6d8f73c80990dd1f56b33eb9138de19af9a7701a
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-24 23:29:25 +0300

    Added background tasks to details view

--- url-shortener/api/api_v1/short_urls/views/details_views.py
+++ url-shortener/api/api_v1/short_urls/views/details_views.py
@@ -1,6 +1,11 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import (
+    APIRouter,
+    Depends,
+    BackgroundTasks,
+    background,
+)
 from starlette import status
 
 from api.api_v1.short_urls.crud import storage
@@ -50,7 +55,9 @@ def read_short_url_details(
 )
 def delete_short_url(
     url: ShortUrlBySlug,
+    background_tasks: BackgroundTasks,
 ) -> None:
+    background_tasks.add_task(storage.save_state)
     storage.delete(short_url=url)
 
 
@@ -61,7 +68,9 @@ def delete_short_url(
 def update_short_url_details(
     url: ShortUrlBySlug,
     short_url_in: ShortUrlUpdate,
+    background_tasks: BackgroundTasks,
 ):
+    background_tasks.add_task(storage.save_state)
     return storage.update(
         short_url=url,
         short_url_in=short_url_in,
@@ -75,7 +84,9 @@ def update_short_url_details(
 def update_short_url_details_partial(
     url: ShortUrlBySlug,
     short_url_in: ShortUrlPartialUpdate,
+    background_tasks: BackgroundTasks,
 ) -> ShortUrl:
+    background_tasks.add_task(storage.save_state)
     return storage.update_partial(
         short_url=url,
         short_url_in=short_url_in,
