8e608c2339556d57f5d07b51617e90f74c383ee7
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-25 22:27:24 +0300

    Added save_storage_state func

--- url-shortener/api/api_v1/short_urls/dependencies.py
+++ url-shortener/api/api_v1/short_urls/dependencies.py
@@ -1,9 +1,16 @@
-from fastapi import HTTPException
+import logging
+
+from fastapi import (
+    HTTPException,
+    BackgroundTasks,
+)
 from starlette import status
 
 from schemas.short_url import ShortUrl
 
 from .crud import storage
+
+log = logging.getLogger(__name__)
 
 
 def prefetch_short_url(
@@ -17,3 +24,11 @@ def prefetch_short_url(
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"URL {slug!r} not found",
     )
+
+
+def save_storage_state(
+    background_tasks: BackgroundTasks,
+):
+    yield
+    log.info("Add background task to save storage")
+    background_tasks.add_task(storage.save_state)
