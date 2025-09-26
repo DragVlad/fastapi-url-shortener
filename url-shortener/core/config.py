c4b8231fa786359a744415b7bc8dca6399b8ba99
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-26 21:34:51 +0300

    Added example API tokens

--- url-shortener/core/config.py
+++ url-shortener/core/config.py
@@ -6,6 +6,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SHORT_URLS_STORAGE_FILEPATH = BASE_DIR / "short-urls.json"
 
 LOG_LEVEL = logging.INFO
+
 LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
+
+API_TOKENS: frozenset[str] = frozenset(
+    {
+        "QiGf4HQPlwDlSAcG0HJ4hg",
+        "VAcHsSPUVKB3WV2FCzp2mg",
+        "4Fds2MAbis2bvLj5-K0dcA",
+    }
 )
