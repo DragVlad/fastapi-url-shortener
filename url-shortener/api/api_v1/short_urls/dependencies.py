3666d27aecc8ceba92c15370ee20e4f8f5c93ade
Vladislav Dragushin <maxhc@inbox.ru>
2025-09-28 19:16:27 +0300

    Updated auth to bearer token

--- url-shortener/api/api_v1/short_urls/dependencies.py
+++ url-shortener/api/api_v1/short_urls/dependencies.py
@@ -10,8 +10,12 @@ from fastapi import (
     HTTPException,
     BackgroundTasks,
     Request,
-    Header,
+    Depends,
     status,
+)
+from fastapi.security import (
+    HTTPAuthorizationCredentials,
+    HTTPBearer,
 )
 
 log = logging.getLogger(__name__)
@@ -24,6 +28,12 @@ UNSAVE_METHODS = frozenset(
         "PATCH",
         "DELETE",
     }
+)
+
+static_api_token = HTTPBearer(
+    scheme_name="Static API token",
+    description="Your **Static API token** from the developer portal. [Read more](#)",
+    auto_error=False,
 )
 
 
@@ -54,12 +64,20 @@ def api_token_required(
     request: Request,
     api_token: Annotated[
         str,
-        Header(alias="x-auth-token"),
-    ] = "",
+        HTTPAuthorizationCredentials | None,
+        Depends(static_api_token),
+    ] = None,
 ):
     if request.method not in UNSAVE_METHODS:
         return
-    if api_token not in API_TOKENS:
+
+    if not api_token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="API token is required",
+        )
+
+    if api_token.credentials not in API_TOKENS:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid API token",
